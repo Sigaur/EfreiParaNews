b763f82cca7481060176de4e9b0ed6188e0b3957
Sigaur <marc.gayraud@efrei.net>
2016-11-03 11:45:25 -0400

    Bookin.php

---
+++ Website/ProductDb/myFunctions.php
@@ -0,0 +1,15 @@
+<?php
+	function printTableRow($productID, $eventType, $begDay, $begMonth, $begYear, $endDay, $endMonth, $endYear, $places)
+	{
+		print("<input type='hidden' name='productID' value='$productID'>");
+		print("<button type=\"button\" class=\"btn btn-primary btn-block buttonWhite\">
+					$eventType:<br>
+					$begDay/$begMonth/$begYear - $begDay/$begMonth/$begYear<br>
+					<br>
+				</button>");
+		print("<td style='border-style: none; text-align: left;'><input type='submit' value='Add'></td>");
+
+	}
+
+
+?>
